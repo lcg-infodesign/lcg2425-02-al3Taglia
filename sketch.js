bbd98804a1e485e7e847b32a6a40bcdafb6400d3
Alessia Tagliaferri <alessia.tagliaferri@mail.polimi.it>
2024-11-04 16:22:02 +0100

    fiocco random

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+let colors=["#dae0e4", "#7fa3b7","#334550","#4d93bb","#0f7ab8","#698392"];
+
 function preload() {
   // put preload code here
 }
@@ -18,20 +20,26 @@ function draw() {
 //---------DISEGNO DI 1! FIOCCO DI NEVE
 
 function drawRametti() { //disegno i ramettini sopra obliqui del fiocco di neve
-  line(-10,-45,0,-40)  
-  line(10,-45,0,-40)
-
-  push();
-  translate(0,10);
-  line(-10,-45,0,-40)
-  line(10,-45,0,-40)
-  pop();
+  let t1=random(-5,5); //variazione random della traslazione
+  let t2=random(-5,5);
+  line(-10,-45+t1,0,-40+t1);  
+  line(10,-45+t1,0,-40+t1);
+  
+  let y=random(0,10);   //ciclo per decidere se fare 1/2 rametti obliqui per ramo
+  if(y<8) {
+    push();
+    translate(0,10);
+    line(-10,-45+t2,0,-40+t2);
+    line(10,-45+t2,0,-40+t2);
+    pop();
+  }
+  
 
 }
 
 function drawRamettiDoppi() {  //disegno i ramettini obliqui del fiocco di neve
   drawRametti();
-
+  
   push();
   rotate(PI);
   drawRametti();
@@ -39,16 +47,20 @@ function drawRamettiDoppi() {  //disegno i ramettini obliqui del fiocco di neve
 }
 
 function drawRamo() { //disegno del ramo del fiocco di neve
+  let color=random(colors);
+  stroke(color);
+  strokeWeight(2);
   line(0,-50,0,50);
   drawRamettiDoppi();
 }
 
 function drawFiocco() { //disegno il fiocco
   translate(75,75);
-  
-  for(let numero_rami=3; numero_rami<7; numero_rami++){
+  let nGiri=random(3,4);
+  for(let numero_rami=0; numero_rami<nGiri; numero_rami++){
     //let angle=random(0,PI)
-    rotate(PI/3)
+    let a=random(-(PI/15),+(PI/15)); //variazione angolo rotazione ramo
+    rotate((PI/nGiri)+a);
     drawRamo();
   }
 }
