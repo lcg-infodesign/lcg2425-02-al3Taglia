7a15e4557912b46dee23054f2b9c1ab5558c6f91
Alessia Tagliaferri <alessia.tagliaferri@mail.polimi.it>
2024-11-04 15:24:05 +0100

    1! fiocco di neve

--- sketch.js
+++ sketch.js
@@ -4,14 +4,51 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background("#b1c7d4");
   // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
-  textAlign(CENTER, CENTER);
-  textSize(16);
-  text(message, width / 2, height / 2);
+  noLoop();
 }
 
 function draw() {
   // put drawing code here
+  drawFiocco();
+}
+
+
+//---------DISEGNO DI 1! FIOCCO DI NEVE
+
+function drawRametti() { //disegno i ramettini sopra obliqui del fiocco di neve
+  line(-10,-45,0,-40)  
+  line(10,-45,0,-40)
+
+  push();
+  translate(0,10);
+  line(-10,-45,0,-40)
+  line(10,-45,0,-40)
+  pop();
+
+}
+
+function drawRamettiDoppi() {  //disegno i ramettini obliqui del fiocco di neve
+  drawRametti();
+
+  push();
+  rotate(PI);
+  drawRametti();
+  pop();
+}
+
+function drawRamo() { //disegno del ramo del fiocco di neve
+  line(0,-50,0,50);
+  drawRamettiDoppi();
+}
+
+function drawFiocco() { //disegno il fiocco
+  translate(75,75);
+  
+  for(let numero_rami=3; numero_rami<7; numero_rami++){
+    //let angle=random(0,PI)
+    rotate(PI/3)
+    drawRamo();
+  }
 }
